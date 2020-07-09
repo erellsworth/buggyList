78a91ff33456ac71e8c6f47d05216124070eaba2
erellsworth <edward.ellsworth@gmail.com>
2020-07-09 19:07:52 -0400

    Fix jacked up routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import {
-    AngularFireAuthGuard,
-    redirectLoggedInTo,
-    redirectUnauthorizedTo
-} from '@angular/fire/auth-guard';
+import { redirectLoggedInTo } from '@angular/fire/auth-guard';
 
 const redirectLoggedIn = () => redirectLoggedInTo(['']);
-const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+
 
 const routes: Routes = [
     {
         path: '',
-        redirectTo: 'tabs',
-        pathMatch: 'full'
-    },
-    {
-        path: 'tabs',
         loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-    },
-    {
-        path: 'tabs',
-        canActivate: [AngularFireAuthGuard],
-        data: { authGuardPipe: redirectUnauthorizedToLogin },
-        children: [
-            {
-                path: 'lists',
-                loadChildren: () => import('./pages/lists/lists.module').then(m => m.ListsPageModule)
-            },
-            {
-                path: 'categories',
-                loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesPageModule)
-            },
-            {
-                path: 'lists/:id',
-                loadChildren: () => import('./pages/list/list.module').then(m => m.ListPageModule)
-            },
-        ]
     },
     {
         path: 'login',
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 
 const routes: Routes = [
     {
         path: 'tabs',
         component: TabsPage,
+        canActivate: [AngularFireAuthGuard],
+        data: { authGuardPipe: redirectUnauthorizedToLogin },
         children: [
             {
                 path: 'lists',
@@ -16,6 +21,10 @@ const routes: Routes = [
                             import('../pages/lists/lists.module').then(m => m.ListsPageModule)
                     }
                 ]
+            },
+            {
+                path: 'lists/:id',
+                loadChildren: () => import('../pages/list/list.module').then(m => m.ListPageModule)
             },
             {
                 path: 'categories',
