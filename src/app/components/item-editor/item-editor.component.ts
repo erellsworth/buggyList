54487aaee39dce0e47dc3c4bc483130f69e55956
erellsworth <edward.ellsworth@gmail.com>
2020-07-14 19:12:55 -0400

    Add item editor

--- src/app/components/item-editor/item-editor.component.ts
+++ src/app/components/item-editor/item-editor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IListItem, IList } from '../../interfaces';
+import { ModalController } from '@ionic/angular';
+import { MemoryHole } from '../../stores/memory-hole';
 
 @Component({
     selector: 'app-item-editor',
@@ -8,11 +10,33 @@ import { IListItem, IList } from '../../interfaces';
 })
 export class ItemEditorComponent implements OnInit {
 
-    @Input() item: IListItem;
-    @Input() list: IList;
+    @Input() item?: IListItem;
 
-    constructor() { }
+    constructor(
+        private modal: ModalController,
+        private store: MemoryHole
+    ) { }
 
     ngOnInit() { }
+
+    public async close(): Promise<void> {
+        await this.modal.dismiss();
+    }
+
+    public colorChanged(color: string): void {
+        if (!this.item) { return; }
+        this.item.color = color;
+    }
+
+    public nameChanged(event: any): void {
+        if (!this.item) { return; }
+
+        this.item.name = event.detail.value;
+    }
+
+    public async update() {
+        await this.store.updateSingle('items', this.item);
+        await this.close();
+    }
 
 }
